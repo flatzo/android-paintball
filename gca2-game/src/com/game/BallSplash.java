ac4dddf545d3411dbe7c08a73b8b86e15f84534b
charlesbriere.flatzo@gmail.com <charlesbriere.flatzo@gmail.com@e84c0eff-dd54-9015-66b7-0f7816ab3cb0>
2011-10-01 19:25:50 +0000

    Hum, ça chill dans le ghetto avec une map sur laquelle on peut se déplacer.

---
+++ gca2-game/src/com/game/BallSplash.java
@@ -0,0 +1,14 @@
+package com.game;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+
+public class BallSplash {
+	
+	Animation mExplosion;
+	Animation mSwipeFromLeft;
+	Animation mSwipeFromRight;
+	
+	public BallSplash() {
+		
+	}
+}
